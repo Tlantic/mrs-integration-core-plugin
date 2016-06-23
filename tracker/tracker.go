1e36abbb40ec940af56533a1ecd04d186ddbe9a6
Andre Pinto <andre40826@gmail.com>
2016-06-23 18:21:25 +0100

    add loggly

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -4,5 +4,5 @@ package tracker
 type Tracker interface {
 	TrackEvent(event string, params map[string]interface{})
 	TrackLog(event string, params map[string]interface{})
-	TrackError(event string, err error, params map[string]interface{})
+	TrackError(event string, params map[string]interface{})
 }
